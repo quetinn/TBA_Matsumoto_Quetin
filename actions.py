5c753d0917e30fbe9a215ad186399e766b8a43db
Lucca M <lucca.matsumoto@edu.esiee.fr>
2024-12-02 13:50:01 +0000

    update historique

--- actions.py
+++ actions.py
@@ -54,62 +54,49 @@ class Actions:
             return False
 
         # Listes des ecritures possibles pour les directions
-        rerA = ["A", "a", "RERA", "rerA", "rera","ligneA"]
-        rerB = ["B", "b", "RERB", "rerB", "rerb","ligneB"]
-        rerBN = ["BN", "bn", "RERBN", "rerBN", "rerbn","B-N","b-n"]
-        rerBC = ["BC", "bc", "RERBC", "rerBC", "rerbc","B-C","b-c"]
-        rerBM = ["BM", "bm", "RERBM", "rerBM", "rerbm", "B-M","b-m"]
-        rerC = ["C", "c", "RERC", "rerC", "rerc","ligneC"]
-        rerE = ["E", "e", "RERE", "rerE", "rere","ligneE"]
-        Mtreize = ["13", "treize", "Treize", "TREIZE", 'metro13', "METRO13", "métro13","M13","ligne13"]
-        Mquatorze = ["14", "quatorze", "Quatorze", "QUATORZE", "metro14", "METRO14", "métro14","M14","ligne14"]
-        MuneD = ["1D", "uneD", "1D", "unD", "metro1D", "METRO1D", "métro1D", "1-D","M1-D","1d"]
-        MuneV = ["1V", "uneV", "1V", "unV", "metro1V", "METRO1V", "métro1V", "1-V", "M1-V","1v"]
-        Surface = ["sortir", "Sortir", "SORTIR"]
-        red = ["redescendre", "Redescendre", "REDESCNDRE"]
-        Catacombes = ["descendre", "DESCENDRE", "Descendre"]
-        rem = ["remonter", "Remonter", "REMONTER"]
-
+        rerA = ["a", "rera","lignea"]
+        rerB = ["b", "rerb","ligneb"]
+        rerBN = ["bn", "rerbn","b-n"]
+        rerBC = ["bc", "rerbc", "b-c"]
+        rerBM = ["bm", "rerbm", "b-m"]
+        rerC = ["c", "rerc","lignec"]
+        rerE = ["e", "rere", "lignee"]
+        Mtreize = ["13", "treize", 'metro13', "métro13","m13","ligne13"]
+        Mquatorze = ["14", "quatorze", "metro14", "métro14","m14","ligne14"]
+        MuneD = ["1d", "uned", "und", "metro1d", "métro1d", "1-d","m1-d"]
+        MuneV = ["1v", "unev", "unv", "metro1v", "métro1v", "1-v", "m1-v"]
 
         # Get the direction from the list of words.
-        if list_of_words[1] in rerA:
+        if list_of_words[1].lower() in rerA:
             direction = "A"
-        elif list_of_words[1] in rerB:
+        elif list_of_words[1].lower() in rerB:
             direction = "B"
-        elif list_of_words[1] in rerBN:
+        elif list_of_words[1].lower() in rerBN:
             direction = "B-N"
-        elif list_of_words[1] in rerBC:
+        elif list_of_words[1].lower() in rerBC:
             direction = "B-C"
-        elif list_of_words[1] in rerBM:
+        elif list_of_words[1].lower() in rerBM:
             direction = "B-M"
-        elif list_of_words[1] in rerC:
+        elif list_of_words[1].lower() in rerC:
             direction = "C"
-        elif list_of_words[1] in rerE:
+        elif list_of_words[1].lower() in rerE:
             direction = "E"
-        elif list_of_words[1] in Mtreize:
+        elif list_of_words[1].lower() in Mtreize:
             direction = "13"
-        elif list_of_words[1] in Mquatorze:
+        elif list_of_words[1].lower() in Mquatorze:
             direction = "14"
-        elif list_of_words[1] in MuneD:
+        elif list_of_words[1].lower() in MuneD:
             direction = "1-D"
-        elif list_of_words[1] in MuneV:
+        elif list_of_words[1].lower() in MuneV:
             direction = "1-V"
-        elif list_of_words[1] in Surface:
-            direction = "sortir"
-        elif list_of_words[1] in red:
-            direction = "redescendre"
-        elif list_of_words[1] in Catacombes:
-            direction = "descendre"
-        elif list_of_words[1] in rem:
-            direction = "remonter"
+        else : 
+            direction = list_of_words[1].lower()
 
         # Move the player in the direction specified by the parameter.
         try:
             player.move(direction)
-        except UnboundLocalError:
-            print("\n Direction inconnue. \n")
         except KeyError:
-            print("\n Ligne indisponible dans cette station. \n")
+            print("\n Direction inconnue. \n")
         return True
 
     def quit(game, list_of_words, number_of_parameters):
--- player.py
+++ player.py
@@ -1,10 +1,3 @@
-# The error message is stored in the MSG0 and MSG1 variables and formatted with the command_word variable, the first word in the command.
-# The MSG0 variable is used when the command does not take any parameter.
-MSG0 = "\nLa commande '{command_word}' ne prend pas de paramètre.\n"
-# The MSG1 variable is used when the command takes 1 parameter.
-MSG1 = "\nLa commande '{command_word}' prend 1 seul paramètre.\n"
-
-
 # Define the Player class.
 class Player():
 
@@ -35,19 +28,11 @@ class Player():
 
         # Set the current room to the next room.
         self.current_room = next_room
-        ##self.previous_room = self.current_room
         self.history.append(self.current_room.name)
-        print(self.current_room.get_long_description() ,"\n", self.get_history())
+        print(self.current_room.get_long_description() ,"\n",self.get_history())
         return True
 
-    def get_history(self, game, list_of_words, number_of_parameters):
-        player = game.player
-        l = len(list_of_words)
-        # If the number of parameters is incorrect, print an error message and return False.
-        if l != number_of_parameters + 1:
-            command_word = list_of_words[0]
-            print(MSG1.format(command_word=command_word))
-            return False
+    def get_history(self, *args):
         history_string = "Vous avez déja visité les endroits suivants :\n"
         for nom in self.history :
             history_string += "\t- " + nom + "\n"
